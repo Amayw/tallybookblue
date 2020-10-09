9e28b1c74199afe37c205f87f781c137bdc9e10a
wywang <727936199@qq.com>
2020-10-09 19:32:08 +0800

    完成Nav组件样式，更新meta viewport。

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="nav">
-        <router-link to="/labels">
+    <nav>
+        <router-link class="item" active-class="selected" to="/labels">
             <Icon name="1" />
             标签
         </router-link>
-        <router-link to="/money">
+        <router-link class="item" active-class="selected" to="/money">
             <Icon name="2"/>
             记账
         </router-link>
-        <router-link to="/statistics">
+        <router-link class="item" active-class="selected" to="/statistics">
             <Icon name="3"/>
             统计
         </router-link>
-    </div>
+    </nav>
 </template>
 
 <script lang="ts">
@@ -22,9 +22,26 @@
 </script>
 
 <style lang="scss" scoped>
-.nav{
+nav{
     display: flex;
     justify-content: space-around;
-    border: 1px solid olivedrab;
+    background: #f6f7f2;
+    >.item{
+        color: #d6d6d6;
+        padding: 6px 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        font-size: 12px;
+        >.icon{
+            width: 26px;
+            height: 26px;
+        }
+    }
+
+    >.item.selected{
+        color: #a3c7bb;
+    }
 }
 </style>
