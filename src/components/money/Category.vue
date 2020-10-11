86311e1d7ec58fc7841dd406dce75ccb2fcbb2d6
wywang <727936199@qq.com>
2020-10-11 14:37:10 +0800

    使用js实现category组件

--- src/components/money/Category.vue
+++ src/components/money/Category.vue
@@ -1,16 +1,26 @@
 <template>
     <div class="category">
-        <ul>
-            <li :class="category==='1'?'active':''">支出</li>
-            <li :class="category==='2'?'active':''">收入</li>
+        <ul  >
+            <li :class="category==='1'?'active':''" @click="changeCategory('1')">支出</li>
+            <li :class="category==='2'?'active':''" @click="changeCategory('2')">收入</li>
         </ul>
     </div>
 </template>
 
-<script lang="ts">
+<script lang="js">
     export default {
         name: 'Category',
-        props:['category']
+        // props:['category'],
+        data(){
+            return {
+                category:'1'//'1'表示支出，'2'表示收入
+            }
+        },
+        methods:{
+            changeCategory(type){
+                this.category = type;
+            }
+        }
     };
 </script>
 
